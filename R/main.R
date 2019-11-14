c4298cf60d6a8147c534174dd1d2c6ce6c1b56cb
Nicole Kauer <nmekauer@gmail.com>
2019-11-14 13:43:08 -0800

    Fix function name error & give default for `state_filter`

--- R/main.R
+++ R/main.R
@@ -4,8 +4,9 @@
 #' @param state_filter The filter that is desired to gather submissions by.
 #' @param group The groupID.
 #' @param output_dir The directory to output the submissions and csv file to.
-download_all_and_output_to_csv <- function(state_filter, group, output_dir) {
+download_all_and_output_to_csv <- function(state_filter = "SUBMITTED_WAITING_FOR_REVIEW", # nolint
+                                           group, output_dir) {
   download_all_submissions(state_filter, group, output_dir)
-  data <- convert_all_submissions_to_table(output_dir)
+  data <- convert_all_forms_to_table(output_dir)
   output_submission_csv(data, output_dir)
 }
