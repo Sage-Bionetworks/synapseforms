0ffaab060e7340e1bde5300b08c11751e26a3597
Nicole Kauer <nmekauer@gmail.com>
2019-11-14 13:43:25 -0800

    Give default to `state_filter`

--- R/download_all_submissions.R
+++ R/download_all_submissions.R
@@ -3,7 +3,8 @@
 #' @param state_filter The filter that is desired to gather submissions by.
 #' @param group The groupID.
 #' @param output_dir The directory to output the files to.
-download_all_submissions <- function(state_filter, group, output_dir) {
+download_all_submissions <- function(state_filter = "SUBMITTED_WAITING_FOR_REVIEW", # nolint
+                                     group, output_dir) {
   synapser::synLogin()
   subs_meta <- get_submissions_metadata(state_filter, group)
   ps_url_list <- purrr::map2(
