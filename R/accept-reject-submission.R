e19e088f2692f821a05b3887c9021efcf59951f3
Nicole Kauer <nmekauer@uw.edu>
2019-12-19 13:57:10 -0800

    Add functions to reject or accept submissions (#12)
    * Add functions to reject or accept submissions
    
    Could use some safety nets still.
    
    * Update documentation to mention throwing errors
    
    Design choice to have it throw the error. The stopadforms Shiny App is one use-case in which throwing the error is desired since the buttons will be set up to display the error message.
    
    * Add warning message

---
+++ R/accept-reject-submission.R
@@ -0,0 +1,55 @@
+#' Reject a submission
+#'
+#' Reject a submission and give a reason. This sets the
+#' submission status `state` to `REJECTED`, and updates the
+#' review elements, `reviewedOn`, `reviewedBy`, and
+#' `rejectionMessage`.` Note: this function will
+#' throw an error if the REST call to Synapse fails; this could
+#' happen if the user does not have needed permissions, or
+#' the `form_data_id` specified does not exist.
+#'
+#' WARNING: Once a submission is rejected, the state can
+#' not be changed again.
+#'
+#' @export
+#' @inheritParams download_all_and_get_table
+#' @param form_data_id The formDataId of the submission.
+#' @param reason The reason for the rejection as a string;
+#'   limited to 500 characters.
+#' @return The response from the rejection call as a list containing
+#'   the submission metadata.
+reject_submission <- function(syn, form_data_id, reason) {
+  if (nchar(reason) <= 500) {
+    body <- glue::glue('{{"reason":"{reason}"}}')
+    reject_response <- syn$restPUT(
+      uri = glue::glue("https://repo-prod.prod.sagebase.org/repo/v1/form/data/{form_data_id}/reject"), # nolint
+      body = body
+    )
+    reject_response
+  } else {
+    stop("The rejection reason is limited to 500 characters")
+  }
+}
+
+#' Accept a submission
+#'
+#' Accept a submission. This sets the submission status
+#' `state` to `ACCEPTED`, and updates the review elements,
+#' `reviewedOn` and `reviewedBy`. Note: this function will
+#' throw an error if the REST call to Synapse fails; this could
+#' happen if the user does not have needed permissions, or
+#' the `form_data_id` specified does not exist.
+#'
+#' WARNING: Once a submission is rejected, the state can
+#' not be changed again.
+#'
+#' @export
+#' @inheritParams reject_submission
+#' @return The response from the acceptance call as a list containing
+#'   the submission metadata.
+accept_submission <- function(syn, form_data_id) {
+  accept_response <- syn$restPUT(
+    uri = glue::glue("https://repo-prod.prod.sagebase.org/repo/v1/form/data/{form_data_id}/accept") # nolint
+  )
+  accept_response
+}
